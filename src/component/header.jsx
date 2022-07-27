2a7d1d417b2638906853ed1bbfddae4a4c9984e7
Sunflwr15 <nabil.gathfan@gmail.com>
2022-07-27 11:32:17 +0700

    ReactGathfan

---
+++ src/component/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Page from '../page'
+function Header() {
+  return (
+    <React.Fragment>
+        <code>
+          <h1>World</h1>
+        </code>
+        <Page />
+    </React.Fragment>
+  );
+}
+
+export default Header;
---
+++ src/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './component/header'
+
+function Page() {
+    return ( 
+        <React.Fragment>
+            <h1>Ini adalah Page</h1>
+        </React.Fragment>
+     );
+}
+
+export default Page;
