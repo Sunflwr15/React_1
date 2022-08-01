8896d44a001623c8bb389ceecdf5a5784e8672e4
Sunflwr15 <nabil.gathfan@gmail.com>
2022-08-01 14:31:22 +0700

    latihan1

--- src/module/isiData.jsx
+++ src/module/isiData.jsx
@@ -6,17 +6,22 @@ function Tampilan({ list }) {
       {list?.map((data, index) => {
         return (
           <React.Fragment>
-            <div>
-              <h4>Jenis: {data.jenis}</h4>
-              <h4>Produk: {data.produk}</h4>
-              <h3>Tipe</h3>
-              <p>Nama: {data.brand[0].nama}</p>
-              <p>Harga: {data.brand[0].harga}</p>
-              <p>Nama: {data.brand[1].nama}</p>
-              <p>Harga: {data.brand[1].harga}</p>
+            <div className="center">
+              <div className="center1">
+                <h4>Jenis: {data.jenis}</h4>
+                <h4>Produk: {data.produk}</h4>
+                <h3>Tipe 1</h3>
+                <ul>
+                  <li>Nama: {data.brand[0].nama}</li>
+                  <li>Harga: {data.brand[0].harga}</li>
+                </ul>
+                <h3>Tipe 2</h3>
+                <ul>
+                  <li>Nama: {data.brand[1].nama}</li>
+                  <li>Harga: {data.brand[1].harga}</li>
+                </ul>
+              </div>
             </div>
-            
-            
           </React.Fragment>
         );
       })}
