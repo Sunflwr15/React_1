17a9c9511bcb1fc5ed12b9aaf92d14432f276de1
Sunflwr15 <nabil.gathfan@gmail.com>
2022-08-03 14:31:05 +0700

    belajar-state-dan-event

--- src/App.js
+++ src/App.js
@@ -3,37 +3,43 @@ import Layout from "./module/layout";
 import Button from "./module/button";
 import "./style/style.css";
 
+// function App() {
+//   let [count, setCount] = React.useState(0);
+//   const handleTambah = () => {
+//     setCount(count + 1);
+//   };
+//   const handleKurang = () => {
+//     setCount(count - 1);
+//   };
+//   const Reset = () => {
+//     setCount(0)
+//   }
+//   return (
+//     <React.Fragment>
+//       <div className="center-fix layout">
+//         <h1>Count = {count}</h1>
+//         <Button onClick={handleTambah} title={"Tambah"} color={"#582eff"} />
+//         <Button disabled={count <= 0 ? true : false} onClick={handleKurang} title={"Kurang"} color={"Crimson"} />
+//         <Button
+//         disabled={count === 0 ? true : false}
+//           onClick={Reset}
+//           title={"Reset"}
+//         />
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
 function App() {
+  let [name, setName] = React.useState("");
+  let [email, setEmail] = React.useState("");
+  let [Password, setPassword] = React.useState("");
+  let [confirmPassword, setConfirmPassword] = React.useState("");
   return (
     <React.Fragment>
-      <h1>Belajar Props</h1>
-      <Layout title={"Asal SMK"}>
-        <h1>SMK MQ</h1>
-      </Layout>
-      <Layout title={"Asal SMK"}>
-        <h1>SMK IDN</h1>
-      </Layout>
-      <Layout title={"Asal SMK"}>
-        <h1>SMK 01</h1>
-      </Layout>
-      {/* <Layout /> */}
-      <div className="flex">
-        <Button
-          onClick={() => {
-            console.log("Starting...");
-          }}
-          title={"Simpan"}
-          color={"#582eff"}
-        />
-        <Button
-          onClick={() => {
-            console.log("Canceling...");
-          }}
-          title={"Batal"}
-          color={"crimson"}
-        />
-        <Button title={"Update"} color={"crimson"} />
-      </div>
+      <form className="center-fix">Tes</form>
     </React.Fragment>
   );
 }
--- src/module/button.jsx
+++ src/module/button.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Button({ title, color, ...props }) {
+function Button({ title, color = "light gray", disabled, ...props }) {
   return (
     <div>
       <button
+        disabled={disabled}
         {...props}
         style={{
           backgroundColor: color,
+          opacity: disabled ? 0.3 : 1,
         }}
         className="button"
       >
