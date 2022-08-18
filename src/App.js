561e221df0cb11e11f570e1a3305cf3726d68d3f
Sunflwr15 <nabil.gathfan@gmail.com>
2022-08-18 11:07:18 +0700

    ReactGathfan

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <React.Fragment>
-        <h1 className="text-3xl font-bold underline m-10 font-mono text-violet-600">
+        <h1 className="font-bold underline m-10 font-mono text-violet-600">
           Hello world!
         </h1>
     </React.Fragment>
