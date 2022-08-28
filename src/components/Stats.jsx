12c1a8aa17ae9ce5920c93577cd86c677d75cbb6
Sunflwr15 <nabil.gathfan@gmail.com>
2022-08-28 10:16:44 +0700

    JSmastery1

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,6 @@
 import { stats } from "../constants";
 import styles from "../style";
 
-
 const Stats = () => (
   <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
     {stats.map((stat) => (
