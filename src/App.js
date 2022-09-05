e40dc7f1da2999613cdcaa547706532c10db7edb
Sunflwr15 <nabil.gathfan@gmail.com>
2022-09-05 11:12:35 +0700

    UjianRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
-  return (
-    <React.Fragment>
-        <h1 className="font-bold underline m-10 font-mono text-violet-600">
-          Hello world!
-        </h1>
-    </React.Fragment>
-  );
+  return <React.Fragment>
+    
+  </React.Fragment>;
 }
 
 export default App;
