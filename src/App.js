4a7e1f83fe089c62203a9c881c6a0a0b5fef2f9d
Sunflwr15 <nabil.gathfan@gmail.com>
2022-09-05 11:25:04 +0700

    UjianRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes, NavLink, Navigate } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import second from "./pages/admin";
 
 function App() {
-  return <React.Fragment>
-    
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <Login />
+
+      <Routes>
+        <Route to="/" element={<Login />}></Route>
+        <Route to="login" element={<Login />}></Route>
+        <Route to="register" element={<Register />}></Route>
+        <Route to="*" element={<Navigate to="/" replace/>}></Route>
+      </Routes>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
 
-
-export default function Login(){
-    return (
-        <div>
-            <h1>Login Page</h1>
-           <div className="flex">
-           
-           </div>
-        </div>
-    )
+export default function Login() {
+    const navigate = useNavigate()
+    const handleRegister = () => {
+        navigate("/register", {replace: true})
+    }
+    const handleLogin = () => {
+        navigate("/login", {replace: true})
+    }
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <div className="flex">
+        <button onClick={handleRegister}>Register</button>
+      </div>
+    </div>
+  );
 }
