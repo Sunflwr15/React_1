89d8cfa9f55c851253109263fd3a53b9460512d5
Sunflwr15 <nabil.gathfan@gmail.com>
2022-09-09 14:42:17 +0700

    alQuran

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -22,11 +22,11 @@ const User = () => {
   console.log("Users =>", users);
   console.log("Page =>", page);
   return (
-    <section className="bg-gray-900 w-screen h-full px-10 py-10">
-      <h1 className="text-[50px] text-white font-bold pb-10">
+    <section className="bg-gray-900 w-screen h-full px-10 py-10 pb-10">
+
+      <h1 className="text-[50px] text-white font-bold pb-10 text-center">
         DAFTAR SURAT<span className="text-violet-500">.</span>
       </h1>
-
       <div className="flex justify-center">
       <section className="grid grid-cols-1 gap-4 xl:grid-cols-2 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
         {users.map((item, index) => {
@@ -45,6 +45,7 @@ const User = () => {
         })}
       </section>
       </div>
+        <p className="text-end text-gray-500 pr-[40px] pt-10">@gathfam.</p>
     </section>
   );
 };
