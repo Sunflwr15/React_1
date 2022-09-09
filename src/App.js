deedb9f13c5e24f66caa1f5eae040218060a4cb5
Sunflwr15 <nabil.gathfan@gmail.com>
2022-09-09 13:17:35 +0700

    API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import User from "./pages/user";
 
 function App() {
   return (
     <React.Fragment>
-        <h1 className="font-bold underline m-10 font-mono text-violet-600">
+      {/* <h1 className="font-bold underline m-10 font-mono text-violet-600">
           Hello world!
-        </h1>
+        </h1> */}
+      <User />
     </React.Fragment>
   );
 }
